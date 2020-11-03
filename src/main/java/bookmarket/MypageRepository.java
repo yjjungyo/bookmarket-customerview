5d25bc1b5561c4a11aea1c35be4230ae9f5c0e40
jaesunyoon <jaesunyoon@sk.com>
2020-11-03 16:23:14 +0900

    init commit

---
+++ src/main/java/bookmarket/MypageRepository.java
@@ -0,0 +1,16 @@
+package bookmarket;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MypageRepository extends CrudRepository<Mypage, Long> {
+
+    List<Mypage> findByOrderId(Long orderId);
+    List<Mypage> findByOrderId(Long orderId);
+    List<Mypage> findByOrderId(Long orderId);
+    List<Mypage> findByOrderId(Long orderId);
+    List<Mypage> findByOrderId(Long orderId);
+
+}
